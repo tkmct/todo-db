179a9cba06e1e87fe38168be19d8ecb44a3f6dca
Takamichi Tsutsumi <1220t.takamichi@gmail.com>
2019-11-11 12:15:47 +0900

    Add command line argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::{App, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("myapp")
+        .subcommand(
+            SubCommand::with_name("add")
+                .about("Add todo.")
+                .arg(Arg::with_name("text").index(1).required(true)),
+        )
+        .subcommand(SubCommand::with_name("show"))
+        .get_matches();
+    if let Some(ref matches) = matches.subcommand_matches("add") {
+        println!("Add item: {}", matches.value_of("text").unwrap());
+    }
+    if let Some(..) = matches.subcommand_matches("show") {
+        println!("Show all todos,");
+    }
 }
